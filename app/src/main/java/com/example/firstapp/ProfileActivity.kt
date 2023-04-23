9b9373cb3ed1d125d531337800f956da6d6a5545
MipzZz <maksimilian.2002@mail.ru>
2023-04-24 02:14:08 +0300

    Not Logic

--- app/src/main/java/com/example/firstapp/ProfileActivity.kt
+++ app/src/main/java/com/example/firstapp/ProfileActivity.kt
@@ -2,10 +2,17 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firstapp.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.imBackProf.setOnClickListener {
+            finish()
+        }
+
     }
 }
